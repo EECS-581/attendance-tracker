bc8dd6b1ad0f7477a4a82f3aac2dd890b7e2d6a1
Emma Nasseri <enasseri02@gmail.com>
2023-09-15 14:32:16 -0500

    updated app's file structure

--- attendance-tracker-app/App.js
+++ attendance-tracker-app/App.js
@@ -1,8 +1,10 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import globalStyles from "./src/styles/globalStyles.js";
 
 export default function App() {
   return (
+    //probably want to update globalStyles to replace styles
     <View style={styles.container}>
       <Text>Welcome to our attendance tracker app!</Text>
       <StatusBar style="auto" />
