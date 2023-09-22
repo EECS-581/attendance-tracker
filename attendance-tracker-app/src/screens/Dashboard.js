8323ba843ac5382f8705b795468d203c70e29841
Emma Nasseri <enasseri02@gmail.com>
2023-09-22 10:03:58 -0500

    addded QR scanner widget

--- attendance-tracker-app/src/screens/Dashboard.js
+++ attendance-tracker-app/src/screens/Dashboard.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { View, Text } from "react-native";
 import globalStyles from "../styles/globalStyles.js";
+import QRScanner from "../components/QRScanner.js";
 
 const Dashboard = () => {
   return (
     <View style={globalStyles.container}>
       <Text style={globalStyles.title}>Dashboard</Text>
+      <QRScanner />
       {/* ... other components */}
     </View>
   );
