96c5f0832d1ea2ad664372debaaa3206da3ea9d5
hudsonhrh <hudsonhrh7@gmail.com>
2023-09-24 18:26:16 -0500

    added mint function

--- packages/website/src/pages/about.jsx
+++ packages/website/src/pages/about.jsx
@@ -1,12 +1,18 @@
 import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
+import { useWeb3Context} from "@shared/contexts/web3Context.js";
+
+
 
 export default function About() {
+  const { mintTest } = useWeb3Context();
   return (
+
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Navbar />
       About Page
       <Footer /> 
+      <button onClick={mintTest}>Mint</button>
     </main>   
   );
 };
