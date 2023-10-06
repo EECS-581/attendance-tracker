75b291d448861318ee9a7e52997a15c5eb776783
Emma Nasseri <enasseri02@gmail.com>
2023-10-06 11:21:52 -0500

    added current token count to insights page and simple card component

--- packages/attendance-tracker-app/src/styles/globalStyles.js
+++ packages/attendance-tracker-app/src/styles/globalStyles.js
@@ -27,6 +27,20 @@ const globalStyles = StyleSheet.create({
     borderRadius: 10, // This will give rounded edges
     flexDirection: "row", // This will arrange child components in a row
   },
+
+  cardStyle: {
+    padding: 10,
+    borderRadius: 5,
+    borderWidth: 2,
+    borderColor: "black",
+    marginBottom: 10,
+    width: "90%",
+    backgroundColor: "#f5f5f5", // default background color, but it will be overridden if a prop is passed
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 10,
+    flexDirection: "row",
+  },
 });
 
 export default globalStyles;
