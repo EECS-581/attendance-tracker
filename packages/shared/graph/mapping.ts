9697b5f0b3615ceac37b1dfc0c33d1ce9931202e
hudonhrh <hudsonhrh7@gmail.com>
2023-10-20 14:30:43 -0500

    upodated subgraph info for classes

--- packages/shared/graph/mapping.ts
+++ packages/shared/graph/mapping.ts
@@ -2,6 +2,29 @@ import { BigInt } from "@graphprotocol/graph-ts";
 import { TokensMinted } from "./generated/AttendanceToken/AttendanceToken";
 import { TokenHolder, MintEvent, Token } from "./generated/schema";
 
+import { EnrolledClass, EnrolledClassSession } from "./generated/Classes/Classes";
+import { Class, ClassSession } from "./generated/schema";
+
+export function handleEnrolledClass(event: EnrolledClass): void {
+  let classEntity = new Class(event.params.classId.toString());
+  classEntity.name = event.params.className;
+  classEntity.classId = event.params.classId;
+  classEntity.timestamp = event.params.time.toI32();
+  classEntity.save();
+}
+
+export function handleEnrolledClassSession(event: EnrolledClassSession): void {
+  let sessionEntity = new ClassSession(event.params.sessionId.toString());
+  sessionEntity.className = event.params.className;
+  sessionEntity.classId = event.params.classId;
+  sessionEntity.sessionId = event.params.sessionId;
+  sessionEntity.timestamp = event.params.time.toI32();
+  sessionEntity.save();
+}
+
+// ... rest of the mapping functions ...
+
+
 export function handleTokensMinted(event: TokensMinted): void {
   // Handle the mint event
   let mintEvent = new MintEvent(event.transaction.hash.toHex());
