9ae8225c8a1f79ecbe1e1cb29ddf8a0c08bbf091
hudonhrh <hudsonhrh7@gmail.com>
2023-10-20 14:34:50 -0500

    made function to check if class session exists

--- packages/shared/graph/mapping.ts
+++ packages/shared/graph/mapping.ts
@@ -22,8 +22,6 @@ export function handleEnrolledClassSession(event: EnrolledClassSession): void {
   sessionEntity.save();
 }
 
-// ... rest of the mapping functions ...
-
 
 export function handleTokensMinted(event: TokensMinted): void {
   // Handle the mint event
