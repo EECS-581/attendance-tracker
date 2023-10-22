8ed65a0e85b427ced2d0ca26fbaf40cbf0367229
Emma Nasseri <enasseri02@gmail.com>
2023-10-22 16:29:01 -0500

    got minting animaations to work

--- packages/attendance-tracker-app/src/screens/Scan.js
+++ packages/attendance-tracker-app/src/screens/Scan.js
@@ -22,12 +22,15 @@ import React, { useState, useEffect, useContext } from "react";
 import { Text, View, StyleSheet, Button, Alert } from "react-native";
 import { BarCodeScanner } from "expo-barcode-scanner";
 import CameraButtonContext from "../contexts/CameraButtonContext";
+import LottieView from "lottie-react-native";
 
 export default function Scan() {
   // State to manage camera permission and scan status
   const [hasPermission, setHasPermission] = useState(null);
   const [scanned, setScanned] = useState(false);
   const { setShowCameraButton } = useContext(CameraButtonContext);
+  const [showMintingAnimation, setShowMintingAnimation] = useState(false);
+  const [showSuccessAnimation, setShowSuccessAnimation] = useState(false);
   useEffect(() => {
     setShowCameraButton(false); // Hide the camera button
 
@@ -46,8 +49,17 @@ export default function Scan() {
 
   // Handler for when a QR code is scanned
   const handleBarCodeScanned = ({ type, data }) => {
+    setShowMintingAnimation(true);
+
+    setTimeout(() => {
+      setShowMintingAnimation(false);
+      setShowSuccessAnimation(true);
+    }, 4000);
+
+    setTimeout(() => {
+      setShowSuccessAnimation(false);
+    }, 8000); // Assuming successAnimation lasts for 4 seconds
     setScanned(true);
-    Alert.alert(`QR code data: ${data}`);
   };
 
   // Display message while waiting for permission status
@@ -60,6 +72,7 @@ export default function Scan() {
   }
 
   // Render the QR code scanner
+  //...
   return (
     <View style={styles.container}>
       <BarCodeScanner
@@ -67,8 +80,31 @@ export default function Scan() {
         style={StyleSheet.absoluteFillObject}
       />
 
-      {scanned && (
-        <Button title={"Tap to Scan Again"} onPress={() => setScanned(false)} />
+      {showMintingAnimation && (
+        <LottieView
+          source={require("../../assets/animations/mintingTokenAnimation.json")}
+          autoPlay
+          loop={true}
+          style={styles.lottieAnimation}
+        />
+      )}
+
+      {showSuccessAnimation && (
+        <LottieView
+          source={require("../../assets/animations/tokenMintedSuccess.json")} // Path to your success animation
+          autoPlay
+          loop={false}
+          onAnimationFinish={() => setShowSuccessAnimation(false)}
+          style={styles.lottieAnimation}
+        />
+      )}
+
+      {scanned && !showMintingAnimation && !showSuccessAnimation && (
+        <Button
+          title={"Tap to Scan Again"}
+          color={"white"}
+          onPress={() => setScanned(false)}
+        />
       )}
     </View>
   );
@@ -80,5 +116,11 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: "column",
     justifyContent: "center",
+    alignItems: "center", // Add this line to center child components horizontally
+  },
+  lottieAnimation: {
+    position: "absolute",
+    width: 300, // adjust width
+    height: 300, // adjust height
   },
 });
