583b3b1ad0d4637f1b57c2bcb3008b49ea0f9184
hudonhrh <hudsonhrh7@gmail.com>
2023-10-27 16:34:15 -0500

    classes load in to manage classes now

--- website/src/pages/instructor_dashboard/manage_classes.jsx
+++ website/src/pages/instructor_dashboard/manage_classes.jsx
@@ -1,70 +1,67 @@
-// This code creates the manage classes page for the website
-// Programmers name: Libby Miller
-// Date: 10/17/2023
-// This pages sets up the UI, there are no pre or post conditions, and no inputs to this page
-
-// import necessary components
-// import Navbar component
+import React, { useEffect, useState } from 'react';
 import Navbar from "@/components/navbar";
-// import Footer component
 import Footer from "@/components/footer";
-// import button components
 import LightColorfulButton from "@/components/LightColorfulButton";
 import SolidColorButton from "@/components/SolidColorButton";
+import { useWeb3Context } from "../../contexts/web3Context";
+import { useGraphContext } from "../../contexts/graphContext";
 
-// creates a static list of coupons - this will need to be updated when we are pulling from database
-var listItems = ['Class 1', 'Class 2', 'Class 3', 'Class 4', 'Class 5'];
-
-// create the page manage classes
 export default function Manage_Classes() {
+  const [classes, setClasses] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const { createClassSession, createClass, userWallet } = useWeb3Context();
+  const { queryClassesByTeacher } = useGraphContext(); // Use the queryClassesByTeacher function
+
+  useEffect(() => {
+    console.log(userWallet);
+    async function fetchClasses() {
+      const classes = await queryClassesByTeacher(userWallet);
+      if(classes) {
+        setClasses(classes);
+        setLoading(false);
+      }
+      
+    }
+
+    fetchClasses();
+  }, [userWallet]);
+
   return (
-    // create main html to hold the page
     <main className="w-full overflow-hidden">
       <div className="py-6">
         <div className="container mx-auto">
-        {/* This adds the navbar component to the page */}
           <Navbar />
         </div>
       </div>
       <div className="container mx-auto mt-6 px-4">
-          {/* creates two headers for the page title and subtitle */}
-          <h1 className="text-3xl font-semibold mb-2">Instructor Dashboard</h1>
-          <h2 className="text-2xl font-medium mb-6">Manage Classes</h2>
-          <div className="flex space-x-4 mb-4">
-            {/* creates the Add new class button, this will need to be updated once we have CRUD functionality */}
-            <SolidColorButton title="Add New Class" link="/" />
-          </div>
-          {/* creates an html list, this will need to be refactored when pulling real data */}
+        <h1 className="text-3xl font-semibold mb-2">Instructor Dashboard</h1>
+        <h2 className="text-2xl font-medium mb-6">Manage Classes</h2>
+        <div className="flex space-x-4 mb-4">
+          <SolidColorButton title="Add New Class" link="/" />
+        </div>
+        {loading ? (
+          <p>Loading classes...</p>
+        ) : classes.length > 0 ? (
           <ul className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-          {listItems.map((listItem, index) => (
-            // create a item in list for each element 
-            <li key={index} className="bg-white p-4 shadow-md rounded-md">
-              <div className="flex justify-between items-center">
-                <span className="text-lg font-semibold">{listItem}</span>
-                <div className="space-x-2">
-                  {/* for each item create a button to update class  */}
-                  <LightColorfulButton
-                    shadowColor="#D1CDC7"
-                    title="Update"
-                    link="/"
-                  />
-                  {/* for each item create a button to delete class  */}
-                  <LightColorfulButton
-                    shadowColor="#D1CDC7"
-                    title="Delete"
-                    link="/"
-                  />
+            {classes.map((classItem, index) => (
+              <li key={index} className="bg-white p-4 shadow-md rounded-md">
+                <div className="flex justify-between items-center">
+                  <span className="text-lg font-semibold">{classItem.name}</span>
+                  <div className="space-x-2">
+                    <LightColorfulButton shadowColor="#D1CDC7" title="Update" link="/" />
+                    <LightColorfulButton shadowColor="#D1CDC7" title="Delete" link="/" />
+                  </div>
                 </div>
-              </div>
-            </li>
+              </li>
             ))}
           </ul>
-        </div>
-      {/* This section creates a container to hold the footer */}
+        ) : (
+          <p>No classes available</p>
+        )}
+      </div>
       <div className="py-4 text-center text-sm text-gray-600">
-        {/* This creates an instance of the footer component */}
         <Footer />
-      </div> 
+      </div>
     </main>
   );
-};
+}
