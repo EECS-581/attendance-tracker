82d2d01fa2d1ce77afa9c519fbb1929f34595109
Elisabeth Miller <libbymiller09@gmail.com>
2023-10-29 12:01:06 -0500

    add modals, restyle dashboard to use cards

--- website/src/pages/instructor_dashboard/view_classes.jsx
+++ website/src/pages/instructor_dashboard/view_classes.jsx
@@ -5,23 +5,53 @@
 // Date: 09/24/2023
 // Updated: 09/26/2023
 // Updated: 10/17/2023, Requirement 36.7 - CSS styling version 1 - added tailwind css classes for styling
-// This pages sets up the UI, there are no pre or post conditions, and no inputs to this page
+// Updated: 10/26/2023 - added support for modal for each of the buttons
+// This pages sets up the UI, there are no pre or post conditions
+// inputs to this page are the users addition, deletion, or updating of class items
 
 // import the necessary components
+import React, { useState } from 'react';
 // import Navbar component
 import Navbar from "@/components/navbar";
-// import button component
-import SolidColorButton from "@/components/SolidColorButton";
-// import button component
-import LightColorfulButton from "@/components/LightColorfulButton";
 // import Footer component
 import Footer from "@/components/footer";
-
+import ClassModal from '@/components/ClassModal';
 // creates a static list of classes - this will need to be updated when we are pulling from database
-var listItems = ['Class 1', 'Class 2', 'Class 3', 'Class 4', 'Class 5'];
+var listItems = ['EECS 101', 'EECS 581', 'EECS 658', 'EECS 388', 'EECS 268'];
 
 // creates the View Classes page
 export default function View_Classes() {
+
+   // sets up variables for the add, update and delete button modals with a default value of false
+   const [isUpdateModalOpen, setUpdateModalOpen] = useState(false);
+   const [isDeleteModalOpen, setDeleteModalOpen] = useState(false);
+   const [isAddModalOpen, setAddModalOpen] = useState(false);
+ 
+   // creates openUpdateModal method, updates modal open status
+   const openUpdateModal = () => {
+     setUpdateModalOpen(true);
+   };
+   // creates mehtod to close the modal
+   const closeUpdateModal = () => {
+     setUpdateModalOpen(false);
+   };
+   // creates method to open delete modal
+   const openDeleteModal = () => {
+     setDeleteModalOpen(true);
+   };
+   // creates method to close delete modal
+   const closeDeleteModal = () => {
+     setDeleteModalOpen(false);
+   };
+   // creates method to close add modal
+   const closeAddModal = () => {
+     setAddModalOpen(false);
+   };
+   // creates method to open add modal
+   const openAddModal = () => {
+     setAddModalOpen(true);
+   };
+
   return (
     //creates a main html tag to hold the page 
     <main className="w-full overflow-hidden">
@@ -36,9 +66,15 @@ export default function View_Classes() {
           <h2 className="text-2xl font-medium mb-6">View Classes</h2>
           <div className="flex space-x-4 mb-4">
             {/* creates the Add new class button, this will need to be updated once we have CRUD functionality */}
-            <SolidColorButton title="Add New Class" link="/" />
-            {/* create manage classes button  */}
-            <SolidColorButton title="Manage Classes" link="/instructor_dashboard/manage_classes" />
+            <div 
+            onClick={openAddModal}
+            className='inline-flex items-center justify-center bg-white rounded-lg border border-black p-2 cursor-pointer font-bold relative top-[-3px] left-[-3px] no-underline text-black'
+            style={{
+              backgroundColor: "pink"
+            }}
+            >
+              Add New Class
+            </div>
           </div>
           {/* creates an html list, this will need to be refactored when pulling real data */}
           <ul className="grid grid-cols-1 sm:grid-cols-2 gap-4">
@@ -50,21 +86,25 @@ export default function View_Classes() {
                 <span className="text-lg font-semibold">{listItem}</span>
                 {/* create a button for updating the class  */}
                 <div className="space-x-2">
-                  <LightColorfulButton
-                    shadowColor="#D1CDC7"
-                    title="Update"
-                    link="/"
-                  />
+                  <div onClick={openUpdateModal} className="inline-flex items-center justify-center bg-white rounded-lg border border-black p-2 cursor-pointer font-bold relative top-[-3px] left-[-3px] no-underline text-black">
+                    Update 
+                  </div>
                   {/* create a button for deleting the class  */}
-                  <LightColorfulButton
-                    shadowColor="#D1CDC7"
-                    title="Delete"
-                    link="/"
-                  />
+                  <div onClick={openDeleteModal} className="inline-flex items-center justify-center bg-white rounded-lg border border-black p-2 cursor-pointer font-bold relative top-[-3px] left-[-3px] no-underline text-black">
+                    Delete 
+                  </div>
                 </div>
               </div>
             </li>
             ))}
+            {
+              <div>
+              {/* this handles the closing of all three modals  */}
+              <ClassModal isOpen={isAddModalOpen} onClose={closeAddModal} actionType={"add"} />
+              <ClassModal isOpen={isUpdateModalOpen} onClose={closeUpdateModal} actionType={"update"} />
+              <ClassModal isOpen={isDeleteModalOpen} onClose={closeDeleteModal} actionType={"delete"} />
+            </div>
+            }
           </ul>
         </div>
       </div>
