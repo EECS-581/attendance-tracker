555431737cc8bdd7bb5c336d5d0deb8f2bcb819f
Emma Nasseri <enasseri02@gmail.com>
2023-12-03 22:54:05 -0600

    added account page changes

---
+++ attendance-tracker-app/src/screens/tempData.js
@@ -0,0 +1,23 @@
+export const TempClassData = [
+  {
+    id: 1,
+    className: "EECS 581",
+    instructorName: "David Johnson",
+    classesAttended: 5,
+    totalClassSessionsHeld: 10,
+  },
+  {
+    id: 2,
+    className: "EECS 111",
+    instructorName: "Dr. Instructor ",
+    classesAttended: 2,
+    totalClassSessionsHeld: 10,
+  },
+  {
+    id: 3,
+    className: "EECS 555",
+    instructorName: "Dr. Instructor ",
+    classesAttended: 10,
+    totalClassSessionsHeld: 10,
+  },
+];
